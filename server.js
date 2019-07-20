b7d0bab9f6532aa42139c6a002d39ce392cd0a7d
Scottlove <haleyscottlove@gmail.com>
2019-07-20 16:18:47 -0700

    added html view

--- server.js
+++ server.js
@@ -4,9 +4,25 @@ const app = express();
 const PORT = 3000;
 
 // SECTION MIDDLEWARE
+app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
+
+// SECTION HTML ENDPOINTS
+app.get('/', (req, res) => {
+    res.sendFile(`${__dirname}/views/index.html`);
+});
+
+
+app.post('/', (req, res) => {
+    
+});
+
 
 // SECTION ROUTES
 
-// SECTION HTML ENDPOINTS
+
 
 // SECTION SERVER LISTNER
+app.listen(PORT, () => {
+    console.log(`NOW CONNECTED TO PORT ${PORT}`);
+});
