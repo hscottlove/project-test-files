e098585571a285688e3fa99004a980fb2a5094df
Scottlove <haleyscottlove@gmail.com>
2019-07-20 16:12:05 -0700

    added express

---
+++ server.js
@@ -0,0 +1,12 @@
+// SECTION GLOBAL VARIABLES
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+// SECTION MIDDLEWARE
+
+// SECTION ROUTES
+
+// SECTION HTML ENDPOINTS
+
+// SECTION SERVER LISTNER
